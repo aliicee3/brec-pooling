corrected time computation

Pooling/test_BREC.py
@@ -371,7 +371,7 @@ def main():
         cfi_scores.append(cfi_score)
         # Log average scores and standard deviations
         logger.add(f"{path}/final.txt", format="{message}", encoding="utf-8")
-        logger.info(f'Time for run {i}: {(time.time()-start)%60}')
+        logger.info(f'Time for run {i}: {time.time() - start:.2f} seconds')
     logger.info(
         f'Final max results: {np.max(basic_scores)+np.max(regular_scores)+np.max(ext_scores)+np.max(cfi_scores)}, {np.max(basic_scores)}, {np.max(regular_scores)}, {np.max(ext_scores)}, {np.max(cfi_scores)}')
 